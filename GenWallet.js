3697017ab9a8985dc1f13af5542b5f0b933cabd4
dex2code <90690143+dex2code@users.noreply.github.com>
2024-08-02 14:55:42 +0300

    Update GenWallet.js

--- GenWallet.js
+++ GenWallet.js
@@ -1,46 +1,63 @@
-const { base58Encode, varintEncode } = require('@massalabs/massa-web3/dist/cmd/utils/Xbqcrypto');
-const ed = require('@noble/ed25519');
-const { SecretKey, PublicKey, Address } = require('@massalabs/massa-web3/dist/cmd/utils/keyAndAddresses');
+import * as ed from "@noble/ed25519";
+import { IAccount } from "@massalabs/massa-web3";
+import { varintEncode, base58Encode, hashBlake3 } from "@massalabs/massa-web3/dist/esm/utils/Xbqcrypto"
+import { SecretKey, Address } from "@massalabs/massa-web3/dist/cmd/utils/keyAndAddresses"
 
-const SECRET_KEY_PREFIX = 'S';
+const SECRET_KEY_PREFIX = "S";
+const ADDRESS_PREFIX = "AU"
 const VERSION_NUMBER = 0;
-const ADDRESS_PREFIX_USER = 'AU';
 
-async function walletGenerateNewAccount() {
-  // Generate private key
-  const secretKeyArray = ed.utils.randomPrivateKey();
+async function getNewWallet(): Promise<IAccount> {
+   const version = Buffer.from(
+      varintEncode(VERSION_NUMBER)
+   );
 
-  // Encode the private key
-  const version = Buffer.from(varintEncode(VERSION_NUMBER));
-  const secretKeyBase58Encoded = SECRET_KEY_PREFIX + base58Encode(Buffer.concat([version, secretKeyArray]));
+   const secretKeyArray = ed.utils.randomPrivateKey();
+   const secretKeyBase58Encoded =
+      SECRET_KEY_PREFIX +
+      base58Encode(
+         Buffer.concat(
+            [
+               version,
+               secretKeyArray
+            ]
+         )
+      );
+   const secretKey = new SecretKey(secretKeyBase58Encoded);
 
-  // Create SecretKey instance
-  const secretKey = new SecretKey(secretKeyBase58Encoded);
+   const publicKey = await secretKey.getPublicKey();
 
-  // Get public key
-  const publicKey = await secretKey.getPublicKey();
-  console.log('Generated PublicKey:', publicKey);
+   // "AU" + base58Check(version_bytes + hashBlake3(version_bytes + public_key_bytes))
+   const addressBase58Encoded =
+      ADDRESS_PREFIX +
+      base58Encode(
+         Buffer.concat(
+            [
+               version,
+               hashBlake3(
+                  Buffer.concat(
+                     [
+                        version,
+                        publicKey.bytes
+                     ]
+                  )
+               )
+            ]
+         )
+      );
+   const address = new Address(addressBase58Encoded);
 
-  // Ensure public key has correct prefix
-  const publicKeyBase58Encoded = publicKey.base58Encoded;
-  const addressBase58Encoded = ADDRESS_PREFIX_USER + publicKeyBase58Encoded.slice(2); // Remove the first 2 characters 'P1' and add 'AU'
-
-  // Create Address instance
-  try {
-    const address = new Address(addressBase58Encoded);
-    console.log('Generated Address:', address);
-
-    return {
+   return {
       address: address.base58Encoded,
-      secretKey: secretKeyBase58Encoded,
-      publicKey: publicKeyBase58Encoded,
-    };
-  } catch (error) {
-    console.error('Error creating Address instance:', error);
-  }
+      publicKey: publicKey.base58Encoded,
+      secretKey: secretKeyBase58Encoded
+   } as IAccount;
 }
 
-// Example usage
-walletGenerateNewAccount()
-  .then(account => console.log('New account generated:', account))
-  .catch(err => console.error('Error generating new account:', err));
+getNewWallet()
+   .then(
+      newAccount => { console.log(newAccount); }
+   )
+   .catch(
+      err => { console.log(err); }
+   )
