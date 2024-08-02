95aeccc6e0cf61e4a681676c8b0ddfa03b4d7a60
ckorodenis <35576615+ckorodenis@users.noreply.github.com>
2024-08-02 18:04:57 +0200

    Update to JavaScript
    Transcription of TypeScript into pure JavaScript

--- GenWallet.js
+++ GenWallet.js
@@ -1,63 +1,35 @@
-import * as ed from "@noble/ed25519";
-import { IAccount } from "@massalabs/massa-web3";
-import { varintEncode, base58Encode, hashBlake3 } from "@massalabs/massa-web3/dist/esm/utils/Xbqcrypto"
-import { SecretKey, Address } from "@massalabs/massa-web3/dist/cmd/utils/keyAndAddresses"
+const ed = require("@noble/ed25519");
+const { varintEncode, base58Encode, hashBlake3 } = require("@massalabs/massa-web3/dist/cmd/utils/Xbqcrypto");
+const { SecretKey, Address } = require("@massalabs/massa-web3/dist/cmd/utils/keyAndAddresses");
 
 const SECRET_KEY_PREFIX = "S";
-const ADDRESS_PREFIX = "AU"
+const ADDRESS_PREFIX = "AU";
 const VERSION_NUMBER = 0;
 
-async function getNewWallet(): Promise<IAccount> {
-   const version = Buffer.from(
-      varintEncode(VERSION_NUMBER)
-   );
+async function getNewWallet() {
+   const version = Buffer.from(varintEncode(VERSION_NUMBER));
 
    const secretKeyArray = ed.utils.randomPrivateKey();
-   const secretKeyBase58Encoded =
-      SECRET_KEY_PREFIX +
-      base58Encode(
-         Buffer.concat(
-            [
-               version,
-               secretKeyArray
-            ]
-         )
-      );
+   const secretKeyBase58Encoded = SECRET_KEY_PREFIX + base58Encode(Buffer.concat([version, secretKeyArray]));
    const secretKey = new SecretKey(secretKeyBase58Encoded);
 
    const publicKey = await secretKey.getPublicKey();
 
    // "AU" + base58Check(version_bytes + hashBlake3(version_bytes + public_key_bytes))
-   const addressBase58Encoded =
-      ADDRESS_PREFIX +
-      base58Encode(
-         Buffer.concat(
-            [
-               version,
-               hashBlake3(
-                  Buffer.concat(
-                     [
-                        version,
-                        publicKey.bytes
-                     ]
-                  )
-               )
-            ]
-         )
-      );
+   const addressBase58Encoded = ADDRESS_PREFIX + base58Encode(Buffer.concat([version, hashBlake3(Buffer.concat([version, publicKey.bytes]))]));
    const address = new Address(addressBase58Encoded);
 
    return {
       address: address.base58Encoded,
       publicKey: publicKey.base58Encoded,
       secretKey: secretKeyBase58Encoded
-   } as IAccount;
+   };
 }
 
 getNewWallet()
-   .then(
-      newAccount => { console.log(newAccount); }
-   )
-   .catch(
-      err => { console.log(err); }
-   )
+   .then(newAccount => {
+      console.log(newAccount);
+   })
+   .catch(err => {
+      console.log(err);
+   });
